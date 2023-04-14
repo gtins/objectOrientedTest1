2c8f3365c61ecc9ce22f639ca769ff1179ce0a16
Gustavo <gu.martins@catolicasc.edu.br>
2023-04-14 19:33:16 -0300

    Teste

---
+++ src/Aluno.java
@@ -0,0 +1,9 @@
+public class Aluno extends Pessoa {
+    private int matricula;
+    public int getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(int novaMatricula) {
+        matricula = novaMatricula;
+    }
+}
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,23 @@
 public class Pessoa {
+    private String nome;
+    private int idade;
+    private char sexo;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String mudaNome) {
+        nome = mudaNome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int mudaIdade) {
+        idade = mudaIdade;
+    }
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char mudaSexo) {
+        sexo = mudaSexo;
+    }
 }
